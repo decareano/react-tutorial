99f3f3b6cb7c4f25da0991c74312f18ba1b00186
Marcelo Gobelli <marcelo@marcelos-mbp.gateway.sonic.net>
2021-07-05 13:18:46 -0700

    add state and post a form

--- src/App.js
+++ src/App.js
@@ -1,52 +1,17 @@
 import React, {Component} from 'react'
 import Table from './Table'
+import Form from './Form'
 
 
 
 class App extends Component {
   state = {
-    characters: [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-      {
-        name: 'Marcelo',
-        job: 'Developer',
-      },
-      {
-        name: 'Bob',
-        job: 'Teacher',
-      },
-      {
-        name: 'Ali',
-        job: 'healthcare worker',
-      },
-      {
-        name: 'Nacho',
-        job: 'markey developer',
-      },
-    ],
+    characters: [],
   }
-
-
 
 
   removeCharacter = (index) => {
     const {characters} = this.state
-
     this.setState({
       characters: characters.filter((character, i) => {
         return i !== index
@@ -56,13 +21,16 @@ class App extends Component {
 
   render() {
     const { characters } = this.state
-
     return (
       <div className="container">
-        <Table characterData={characters} 
-        removeCharacter={this.removeCharacter} />
-      </div>
-    );
+      <Table characterData={characters} removeCharacter={this.removeCharacter} />
+      <Form handleSubmit={this.handleSubmit} />
+      </div> 
+    )
+  }
+
+  handleSubmit = (character) => {
+    this.setState({characters: [...this.state.characters, character]})
   }
 }
 
