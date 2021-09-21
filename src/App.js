5b816c78731cc47fab0859bdc93cd3c224c6add9
Marcelo Gobelli <marcelo@marcelos-mbp.gateway.sonic.net>
2021-09-20 18:40:55 -0700

    Employees.js is working on its own file --needs to be working in App.js as well

--- src/App.js
+++ src/App.js
@@ -3,29 +3,18 @@ import Table from './Table'
 import Form from './Form'
 import config from './config'
 import Firebase from 'firebase'
-import employees from './employees'
+import Employees from './employees'
 
 
 
-function App({ query }) {
+function App() {
   
   
-  const [employees, setEmployees] = useState([]);
-
-  useEffect(() => {
-    async function fetchEmployees() {
-      const response = await fetch(
-        `/employees?q=${encodeURIComponent(query)}`
-      );
-      const fetchedEmployees = await response.json(response);
-      setEmployees(fetchedEmployees);
-    }
-    fetchEmployees();
-  }, [query]);
-
+  
   return (
     <div>
-      {employees.map(name => <div>{name}</div>)}
+      
+     
     </div>
   );
 }
--- src/employees.js
+++ src/employees.js
@@ -1,7 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+
 
 function Employee(props) {
+    //will accept the list of employees as a Paramenter (props)
+    // returns a div container for employees details
     return <div style={{border:"3px solid red"}}>
    
     <p>
@@ -20,35 +23,33 @@ function Employee(props) {
     </p>
     </div>
 }
-//employeeList is the props that passes the data 
+
+
+function DisplayEmployees(props) {
+    //employeeList is the prop name as in props.employeeList, it can be anything really
+    const empList = props.empleadosLista
+    console.log(props.empleadosLista)
+    // we loop through list using map() and assign the output to a  variable (listElements)
+    const listElements=empList.map((mpe) =>
+        // pick a key that is unique, ie: ID and then data: defines the other properties of the object
+        
+        <Employee key={mpe.Id} data={mpe}></Employee>
+        
+    )
+    return(
+        <div>
+            {/* //return the contents of listElements */}
+            {listElements}
+        </div>
+    )
+}
 
 const employees = [
     {Id:101,Name:'Abhinav',Location:'Bangalore',Salary:12345},
     {Id:102,Name:'Abhishek',Location:'Chennai',Salary:23456},
     {Id:103,Name:'Ajay',Location:'Bangalore',Salary:34567}
 ]
-
-function DisplayEmployees(props) {
-    const empList=props.employeeList
-    console.log(empList)
-    const listElements=empList.map((emp) =>
-        <Employee key={emp.Id} data={emp}></Employee>
-    )
-    return(
-        <div>
-            {listElements}
-        </div>
-    )
-}
-
-
     
-const element = <DisplayEmployees employeeList={employees}></DisplayEmployees> 
+const element = <DisplayEmployees empleadosLista={employees}></DisplayEmployees> 
     
-ReactDOM.render(
-    <element />, 
-    document.getElementById("root")
-)
-    
-  
-  
+ReactDOM.render(element, document.getElementById("root"))
