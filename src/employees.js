303e8b7eff42573e5822ab25acdd0da80c40958a
Marcelo Gobelli <marcelo@marcelos-mbp.gateway.sonic.net>
2021-09-22 16:51:54 -0700

    code my own props from scratch following tania's tutorial

--- src/employees.js
+++ src/employees.js
@@ -1,61 +1,55 @@
 import React from 'react'
 
-import ReactDOM from 'react-dom';
 
-
-function Employee(props) {
-    //will accept the list of employees as a Paramenter (props)
-    // returns a div container for employees details
-    return <div style={{border:"3px solid red"}}>
-   
-    <p>
-        <label>Employee ID : <b>{props.data.Id}</b></label>
-    </p>
-    <p>
-        <label>Employee Name : <b>{props.data.Name}</b></label>
-    </p>
-
-    <p>
-        <label>Employee Location : <b>{props.data.Location}</b></label>
-    </p>
-
-    <p>
-        <label>Employee Salary : <b>{props.data.Salary}</b></label>
-    </p>
-    </div>
-}
-
-
-function DisplayEmployees(props) {
-    //employeeList is the prop name as in props.employeeList, it can be anything really
-    const empList = props.empleadosLista
-    console.log(props.empleadosLista)
-    // we loop through list using map() and assign the output to a  variable (listElements)
-    const listElements=empList.map((mpe) =>
-        // pick a key that is unique, ie: ID and then data: defines the other properties of the object
-        
-        <Employee key={mpe.Id} data={mpe}></Employee>
-        
+const EmpHeader = () => {
+    return (
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Location</th>
+          <th>Salary</th>
+        </tr>
+      </thead>
     )
-    return(
-        <div>
-            {/* //return the contents of listElements */}
-            {listElements}
-        </div>
+  }
 
+  const EmpBody = (props) => {
+    const list = props.employeeData.map((row, index) => {
+        return (
+            
+            <tr key={index}>
+                <td>{row.id}</td>
+                <td>{row.name}</td>
+                <td>{row.location}</td>
+                <td>{row.salary}</td>
+            </tr>
+            
+        )
+    })
+    
+    return (
+        <tbody>{list}</tbody>
     )
+     
+    
+  }
 
 
+class Employees extends React.Component {
+    render() {
+        const {employeeData} = this.props
+    
+
+    //const {employeeData} = props
+
+    return (
+        <employee>
+            <EmpHeader />
+            <EmpBody employeeData={employeeData} />
+        
+        </employee>
+    )
 }
-
-
-const employees = [
-    {Id:101,Name:'Abhinav',Location:'Bangalore',Salary:12345},
-    {Id:102,Name:'Abhishek',Location:'Chennai',Salary:23456},
-    {Id:103,Name:'Ajay',Location:'Bangalore',Salary:34567}
-]
-    
-const element = <DisplayEmployees empleadosLista={employees}></DisplayEmployees> 
-
-    
-ReactDOM.render(element, document.getElementById("root"))
+}
+export default Employees
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import './index.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router } from "react-router-dom";
 import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
